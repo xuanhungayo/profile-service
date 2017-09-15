8c6da2c7d11ec8c5887b60cd4b80810f38ec3404
Xuan Hung <caoxuanhung96@gmail.com>
2017-09-15 22:19:11 +0700

    Update git ignore

---
+++ inc/ConnectionPool.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ConnectionPool.h
+ * Author: xuanhungayo
+ *
+ * Created on July 22, 2017, 9:24 PM
+ */
+
+#ifndef CONNECTIONPOOL_H
+#define CONNECTIONPOOL_H
+
+class ConnectionPool {
+public:
+	ConnectionPool();
+	ConnectionPool(const ConnectionPool& orig);
+	virtual ~ConnectionPool();
+private:
+
+};
+
+#endif /* CONNECTIONPOOL_H */
+
---
+++ src/ConnectionPool.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ConnectionPool.cpp
+ * Author: xuanhungayo
+ * 
+ * Created on July 22, 2017, 9:24 PM
+ */
+
+#include "ConnectionPool.h"
+
+ConnectionPool::ConnectionPool() {
+}
+
+ConnectionPool::ConnectionPool(const ConnectionPool& orig) {
+}
+
+ConnectionPool::~ConnectionPool() {
+}
+
